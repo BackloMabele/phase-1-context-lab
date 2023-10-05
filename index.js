1cf19d36207d10ffbafd1983a7b027ff87867463
BackloMabele <backlo.mabele@student.moringaschool.com>
2023-10-05 14:45:49 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,5 +1,72 @@
-/* Your Code Here */
+function createEmployeeRecord(employeeArray) {
+    const [firstName, familyName, title, payPerHour] = employeeArray
+    return {
+        firstName,
+        familyName,
+        title,
+        payPerHour,
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
 
+function createEmployeeRecords(arrayOfArrays) {
+    return arrayOfArrays.map((records) => createEmployeeRecord(records))
+}
+
+function createTimeInEvent(dateStamp) {
+    const [date, time] = dateStamp.split(' ');
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(time, 10),
+        date: date
+    }) 
+    return this;
+}
+
+function createTimeOutEvent(dateStamp) {
+    const [date, time] = dateStamp.split(' ');
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(time, 10),
+        date: date
+    })
+    return this
+}
+
+function hoursWorkedOnDate(date) {
+    const timeInEvent = this.timeInEvents.find(event => event.date === date);
+    const timeOutEvent = this.timeOutEvents.find(event => event.date === date);
+  
+    if (!timeInEvent || !timeOutEvent) {
+      return 0;
+    } else {
+        return (timeOutEvent.hour - timeInEvent.hour) / 100;
+    }    
+  }
+
+  function wagesEarnedOnDate(date) {
+    const hoursWorked = hoursWorkedOnDate.call(this, date);
+    const payRate = this.payPerHour;
+    const payOwed = hoursWorked * payRate;
+    return payOwed;
+}
+
+  function calculatePayroll(employeeRecordsArray) {
+    let payroll = 0
+    for(const employeeRecord of employeeRecordsArray) {
+      payroll += allWagesFor.call(employeeRecord)
+    }
+    return payroll
+  }
+
+  function findEmployeeByFirstName(employeeRecords, firstName) {
+    return employeeRecords.find(employeeRecord => employeeRecord.firstName === firstName)
+  }
+  
+  
+  
+  
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
